8e7e1ee48068fd6d5adee2e4775773dc5c0ea82f
vinamcdev <vinamdev@proton.me>
2025-02-09 00:50:06 +0000

    revert(run): removed ability to use zig build run

--- build.zig
+++ build.zig
@@ -1,25 +1,15 @@
 const std = @import("std");
 
-pub fn build(b: std.Build) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "verdis",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
 
     b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_cmd.step);
 }
